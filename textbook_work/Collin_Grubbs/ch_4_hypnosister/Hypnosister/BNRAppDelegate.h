8162b7e6e2844535efea85c3314bdf3649e99162
CollinGrubbs <cbg283@nyu.edu>
2015-03-04 13:47:21 -0500

    completed chapter 4
    upload

---
+++ textbook_work/Collin_Grubbs/ch_4_hypnosister/Hypnosister/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Hypnosister
+//
+//  Created by Nathan Marianovsky on 3/3/15.
+//  Copyright (c) 2015 Collin Grubbs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ textbook_work/Collin_Grubbs/ch_4_hypnosister/Hypnosister/BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by Nathan Marianovsky on 3/4/15.
+//  Copyright (c) 2015 Collin Grubbs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
