7c7d4faee7f0c9ad20f266d020a278c59b80c20e
CollinGrubbs <cbg283@nyu.edu>
2015-03-04 13:50:25 -0500

    completed chapter 7
    upload

---
+++ textbook_work/Collin_Grubbs/ch_7_hypnoNerd/HypnoNerd/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  HypnoNerd
+//
+//  Created by Nathan Marianovsky on 3/4/15.
+//  Copyright (c) 2015 Collin Grubbs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
