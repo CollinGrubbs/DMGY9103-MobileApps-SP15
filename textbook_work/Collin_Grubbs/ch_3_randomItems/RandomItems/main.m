ca0e9cfe980087c1e9d7677368175f8164e356e7
CollinGrubbs <cbg283@nyu.edu>
2015-03-04 13:46:15 -0500

    completed chapter 3
    upload

---
+++ textbook_work/Collin_Grubbs/ch_3_randomItems/RandomItems/main.m
@@ -0,0 +1,43 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Nathan Marianovsky on 2/25/15.
+//  Copyright (c) 2015 Collin Grubbs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BNRItem.h"
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // create a mutable array object, store its address in items variable
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+        
+        BNRItem *backpack = [[BNRItem alloc] initWithItemName:@"Backpack"];
+        [items addObject:backpack];
+        
+        BNRItem *calculator = [[BNRItem alloc] initWithItemName:@"Calculator"];
+        [items addObject:calculator];
+        
+        backpack.containedItem = calculator;
+        
+        backpack = nil;
+        calculator = nil;
+        
+        for (BNRItem *item in items) {
+            NSLog(@"%@", item);
+        }
+        
+        NSLog(@"Setting items to nil...");
+        
+        // destroy the mutable array object
+        items = nil;
+        
+    }
+    return 0;
+}
+
