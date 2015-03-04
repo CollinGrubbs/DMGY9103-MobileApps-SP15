b329dd0e80085c46431e0d3a4c76562f3b7212f1
CollinGrubbs <cbg283@nyu.edu>
2015-03-04 13:49:37 -0500

    completed chapter 6
    upload

---
+++ textbook_work/Collin_Grubbs/ch_6_hypnoNerd/BNRHypnosisViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisViewController.h
+//  HypnoNerd
+//
+//  Created by Nathan Marianovsky on 3/4/15.
+//  Copyright (c) 2015 Collin Grubbs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRHypnosisViewController : UIViewController
+
+@end
