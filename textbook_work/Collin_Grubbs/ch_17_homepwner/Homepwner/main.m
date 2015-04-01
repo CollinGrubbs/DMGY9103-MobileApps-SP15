758b02f60276ee8acdfc805f8c5c356ba182b53a
CollinGrubbs <cbg283@nyu.edu>
2015-04-01 18:10:20 -0400

    backupload 9-17 in progress

---
+++ textbook_work/Collin_Grubbs/ch_17_homepwner/Homepwner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by Nathan Marianovsky on 3/4/15.
+//  Copyright (c) 2015 Collin Grubbs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
